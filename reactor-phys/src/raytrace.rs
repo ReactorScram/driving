5c74e9280d3fd83db4d79206512224f3e536217b
Anonymous <_>
2017-02-28 14:05:01 +0000

    Further refactoring.

--- reactor-phys/src/raytrace.rs
+++ reactor-phys/src/raytrace.rs
@@ -35,7 +35,9 @@ pub fn fold_closer_result (a: Ray2TraceResult, b: Ray2TraceResult) -> Ray2TraceR
 pub fn ray_trace_circle (ray: &Ray2, circle: &Circle) -> Ray2TraceResult {
 	let toward_circle = circle.center - ray.start;
 	
-	if toward_circle * ray.dir < Fx32::from_int (0) {
+	let ray_dot_circle = toward_circle * ray.dir;
+	
+	if ray_dot_circle < Fx32::from_int (0) {
 		// Ray is heading out of the circle's space or is already out
 		return Ray2TraceResult::Miss;
 	}
@@ -51,16 +53,17 @@ pub fn ray_trace_circle (ray: &Ray2, circle: &Circle) -> Ray2TraceResult {
 	
 	// This part is different from the canonical quadratic formula
 	// I negated it because it will save a negation later on
-	let b = (ray.dir * toward_circle) * Fx32::from_int (2);
+	// Then I also halved it
+	let half_b = ray_dot_circle;
 	
 	let c = toward_circle.length_sq () - circle.radius.square_64 ();
 	
-	let determinant = b * b - Fx32::from_int (4) * a * c;
+	let determinant = Fx32::from_int (4) * (half_b.square_64 () - a * c);
 	if determinant < Fx32::from_int (0) {
 		return Ray2TraceResult::Miss;
 	}
 	
-	let t = (b - determinant.sqrt_64 ()) / (Fx32::from_int (2) * a);
+	let t = (half_b * Fx32::from_int (2) - determinant.sqrt_64 ()) / (Fx32::from_int (2) * a);
 	
 	if t >= Fx32::from_int (0) && t <= Fx32::from_int (1) {
 		let ccd_pos = ray.at (t);
